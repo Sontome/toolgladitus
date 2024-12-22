73457b86d9c21a5b0ebc5e753443ebbaabe572ac
Sontome <devilrauxanhk17@gmail.com>
2024-12-22 14:49:24 +0700

    Cập nhật mã nguồn cho gladiatus.py

--- gladiatus.py
+++ gladiatus.py
@@ -7,16 +7,81 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import requests
 import tkinter as tk  # Thêm import tkinter
-from tkinter import messagebox, filedialog  # Thêm import cho messagebox và filedialog
+from tkinter import messagebox, filedialog, ttk  # Thêm import cho messagebox và filedialog
 import threading  # Thêm import threading
 
 # Định nghĩa driver là biến toàn cục
 driver = None
 
 url = 'https://api.telegram.org/bot5737041469:AAG5XdXVwATvldvDpXmnlQT0dmh2-sZ70gE/sendMessage?chat_id=-4153385107&text='
+def checkHp():
+    try:
+        global exp_label
+        global hp_label
+        global hp_progress
+        global wait_time_farm_label
+        global wait_time_label_dungeon
+        global wait_time_label_cistus
+        global wait_time_vodai_label
+        global update_log
+
+        hp_bar_element = driver.find_element(By.ID, "header_values_hp_bar")
+
+        # Lấy giá trị của data-max-value và data-value
+        max_heart = hp_bar_element.get_attribute("data-max-value")
+        heart = hp_bar_element.get_attribute("data-value")
+        hp=f"{heart} / {max_heart}"
+        
+
+        # Cập nhật hiển thị lên phần HP trong giao diện
+        hp_label.config(text=hp)  # Cập nhật nhãn hiển thị HP
+        xp_bar_element = driver.find_element(By.ID, "header_values_xp_bar")
+
+        # Lấy giá trị tooltip
+        tooltip = xp_bar_element.get_attribute("data-tooltip")
+        
+        # Phân tích tooltip để lấy giá trị exp
+        exp_value = tooltip.split('"')[3] 
+         # Lấy giá trị "Experience: 118 / 295"
+        exp_value = exp_value.replace(" / ", " ")
+        exp_percentage = xp_bar_element.find_element(By.ID, "header_values_xp_bar_fill").get_attribute("style").split(":")[1].strip()  # Lấy phần trăm từ style
+        exp_percentage = exp_percentage.replace(";", "")
+        # Kết hợp giá trị exp và phần trăm
+        exp = f"{exp_value} ({exp_percentage})"
+        exp_label.config(text=exp)
+
+        # Cập nhật thanh tiến trình
+        hp_percentage = (int(heart) / int(max_heart)) * 100
+        hp_progress['value'] = hp_percentage  # Cập nhật giá trị thanh tiến trình
+
+        # Cập nhật màu sắc của thanh tiến trình
+        if hp_percentage == 100:
+            hp_progress.config(style='green.Horizontal.TProgressbar')
+        elif hp_percentage >= 50:
+            hp_progress.config(style='yellow.Horizontal.TProgressbar')
+        else:
+            hp_progress.config(style='red.Horizontal.TProgressbar')
+
+        arena_text_element = driver.find_element(By.ID, "cooldown_bar_text_arena")
+        arena_text = arena_text_element.text
+        wait_time_vodai_label.config(text=arena_text)
+        
+        cistus_text_element = driver.find_element(By.ID, "cooldown_bar_text_ct")
+        cistus_text = cistus_text_element.text
+        wait_time_label_cistus.config(text=cistus_text)
+
+        farm_text_element = driver.find_element(By.ID, "cooldown_bar_text_expedition")
+        farm_text = farm_text_element.text
+        wait_time_farm_label.config(text=farm_text)
+
+        dungeon_text_element = driver.find_element(By.ID, "cooldown_bar_text_dungeon")
+        dungeon_text = dungeon_text_element.text
+        wait_time_label_dungeon.config(text=dungeon_text)
+    except:
+        pass
 def di_dau_truong():
-    print('di dau truong')
-    
+    global update_log
+    update_log('di_dau_truong')
     time.sleep(4)
     driver.find_element(By.XPATH, "//*[contains(@onclick, 'startProvinciarumFight(this, 3')]").click()
     time.sleep(4)
@@ -26,9 +91,9 @@ def di_dau_truong():
     # Tìm element có class="report_reward" và lưu nội dung vào biến reward
     reward = driver.find_element(By.CLASS_NAME, "report_reward").text
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"{current_time} - {result}")  
-    print(reward)
+    
     ketqua = f"{current_time} - {result} \n {reward}"
+    update_log(ketqua)
     requests.get(url+ketqua)
 
 def check_dautruong():
@@ -53,16 +118,16 @@ def check_farm():
         pass
 
 def di_farm():
-    print('farm')
-    
+    global update_log
+    update_log('di_farm')
     time.sleep(4)
     driver.find_element(By.XPATH, "//button[contains(@class, 'expedition_button') and contains(@class, 'awesome-button')]").click()
     time.sleep(3)
     try:
         driver.find_element(By.XPATH, "//button[contains(text(), 'Thorough Search')]").click()
-        print("Thorough Search")
+        update_log('Thorough Search')
     except:
-        print("khong co drop them")
+        update_log('khong co drop them')
     time.sleep(4)
     # Tìm element có id là reportHeader và lưu nội dung vào biến result
     result = driver.find_element(By.ID, "reportHeader").text
@@ -70,14 +135,14 @@ def di_farm():
     # Tìm element có class="report_reward" và lưu nội dung vào biến reward
     reward = driver.find_element(By.CLASS_NAME, "report_reward").text
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"{current_time} - {result}") 
-    print(reward)
+    
     ketqua = f"{current_time} - {result} \n {reward}"
+    update_log(ketqua)
     requests.get(url + ketqua)
 
 def di_dungeon():
-    print('di_dungeon')
-    
+    global update_log
+    update_log('di_dungeon')
     time.sleep(4)
     try:
         driver.find_element(By.XPATH, "//input[@class='button1' and @type='submit' and @value='Normal']").click()
@@ -88,9 +153,11 @@ def di_dungeon():
     time.sleep(3)
     try:
         driver.find_element(By.XPATH, "//button[contains(text(), 'Thorough Search')]").click()
-        print("Thorough Search")
+        
+        update_log('Thorough Search')
     except:
-        print("khong co drop them")
+        
+        update_log('khong co drop them')
     
     time.sleep(4)
     # Tìm element có id là reportHeader và lưu nội dung vào biến result
@@ -99,9 +166,9 @@ def di_dungeon():
     # Tìm element có class="report_reward" và lưu nội dung vào biến reward
     reward = driver.find_element(By.CLASS_NAME, "report_reward").text
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"{current_time} - {result}") 
-    print(reward)
+    
     ketqua = f"{current_time} - {result} \n {reward}"
+    update_log(ketqua)
     requests.get(url + ketqua)
 
 def check_dungeon():
@@ -141,6 +208,13 @@ def check_event():
 # Hàm để hiển thị giao diện
 def create_interface():
     global log_text
+    global hp_label
+    global exp_label
+    global hp_progress
+    global wait_time_farm_label
+    global wait_time_label_dungeon
+    global wait_time_label_cistus
+    global wait_time_vodai_label
     global update_log
     global root  # Thêm dòng này để sử dụng biến root toàn cục
     root = tk.Tk()
@@ -173,13 +247,26 @@ def create_interface():
     hp_label = tk.Label(stats_frame, text="0")
     hp_label.grid(row=0, column=1)
 
-    tk.Label(stats_frame, text="EXP:").grid(row=1, column=0)
-    exp_label = tk.Label(stats_frame, text="0")
-    exp_label.grid(row=1, column=1)
+    # Thêm thanh tiến trình cho HP
+    hp_progress = ttk.Progressbar(stats_frame, length=200, mode='determinate')
+    hp_progress.grid(row=1, column=0, columnspan=2)
 
-    tk.Label(stats_frame, text="Thời gian chờ:").grid(row=2, column=0)
-    wait_time_label = tk.Label(stats_frame, text="0")
-    wait_time_label.grid(row=2, column=1)
+    tk.Label(stats_frame, text="EXP:").grid(row=2, column=0)
+    exp_label = tk.Label(stats_frame, text="0")
+    exp_label.grid(row=2, column=1)
+
+    tk.Label(stats_frame, text="Thời gian chờ farm:").grid(row=3, column=0)
+    wait_time_farm_label = tk.Label(stats_frame, text="0")
+    wait_time_farm_label.grid(row=3, column=1)
+    tk.Label(stats_frame, text="Thời gian chờ võ đài:").grid(row=4, column=0)
+    wait_time_vodai_label = tk.Label(stats_frame, text="0")
+    wait_time_vodai_label.grid(row=4, column=1)
+    tk.Label(stats_frame, text="Thời gian chờ dungeon:").grid(row=5, column=0)
+    wait_time_label_dungeon = tk.Label(stats_frame, text="0")
+    wait_time_label_dungeon.grid(row=5, column=1)
+    tk.Label(stats_frame, text="Thời gian chờ Cistus:").grid(row=6, column=0)
+    wait_time_label_cistus = tk.Label(stats_frame, text="0")
+    wait_time_label_cistus.grid(row=6, column=1)
 
     # Khung log
     log_frame = tk.Frame(root)
@@ -204,7 +291,19 @@ def start_auto():
     global update_log
     
     while True:
-        
+        try:
+            driver.find_element(By.XPATH, "//input[@type='submit' and @value='OK']").click()
+        except:
+            pass
+        try:
+            driver.find_element(By.XPATH, "//input[@type='submit' and @value='Confirm']").click()
+        except:
+            pass
+        try:
+            driver.find_element(By.XPATH, "//input[@type='submit' and @value='Close']").click()
+        except:
+            pass
+        checkHp()
         check_dungeon()
         check_dautruong()
         check_farm()
@@ -244,7 +343,7 @@ def login():
         driver.find_element(By.XPATH, '//*[@id="joinGame"]/button').click()
         
         driver.switch_to.window(driver.window_handles[-1]) 
-        time.sleep(6)
+        time.sleep(8)
 
         # Chụp ảnh trình duyệt và lưu vào file check.png
         driver.save_screenshot('check.png')
